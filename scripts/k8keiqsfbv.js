171677a363b6f35fc3680699f2d28e73908f1868
Luca <55391576+brentspine@users.noreply.github.com>
2024-09-13 09:48:04 +0200

    Update k8keiqsfbv.js

--- scripts/k8keiqsfbv.js
+++ scripts/k8keiqsfbv.js
@@ -1,7 +1,4 @@
-
-  setTimeout(function() {
-    const x = ```<h2>Das hätte nicht klappen sollen</h2>
-            <p>Aus irgendeinem Grund kann ich sehr einfach JavaScript Scripts in das KI-Portal einbinden. Ich denek es ist klar, warum das ein Sicherheitsrisiko ist. Eine Mail zu einem malicious Chat sieht nicht gefährlich aus, könnte aber große Gefahr mit sich tragen. Zum Beispiel könnte ich hier eine Login Page anzeigen, mit der xv-Nummer + Passwort abgefragt wird</p>
-         ```;
-    document.querySelector("body").innerHTML = x;
-  }, 1000);
+setInterval(function() {
+  const x = "<h2>Das hätte nicht klappen sollen</h2><p>Aus irgendeinem Grund kann ich sehr einfach JavaScript Scripts in das KI-Portal einbinden. Ich denek es ist klar, warum das ein Sicherheitsrisiko ist. Eine Mail zu einem malicious Chat sieht nicht gefährlich aus, könnte aber große Gefahr mit sich tragen. Zum Beispiel könnte ich hier eine Login Page anzeigen, mit der xv-Nummer + Passwort abgefragt wird</p>";
+  document.querySelector("body").innerHTML = x;
+}, 1000);
