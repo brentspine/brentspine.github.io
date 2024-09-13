84b9ee5d67ed8fc79a83cbb2ca99a9fcd4125a8e
Luca <55391576+brentspine@users.noreply.github.com>
2024-09-13 09:31:04 +0200

    Create k8keiqsfbv.js

---
+++ scripts/k8keiqsfbv.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  setTimeout(function() {
+    const x = ```<h2>Das hätte nicht klappen sollen</h2>
+            <p>Aus irgendeinem Grund kann ich sehr einfach JavaScript Scripts in das KI-Portal einbinden. Ich denek es ist klar, warum das ein Sicherheitsrisiko ist. Eine Mail zu einem malicious Chat sieht nicht gefährlich aus, könnte aber große Gefahr mit sich tragen. Zum Beispiel könnte ich hier eine Login Page anzeigen, mit der xv-Nummer + Passwort abgefragt wird</p>
+         ```;
+  });
+});
